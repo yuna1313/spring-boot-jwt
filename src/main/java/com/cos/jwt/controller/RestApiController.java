6033663ce2faeb911cf49273c2bd9f4bc2afbcb1
yuna1313 <yuna38748435@gmail.com>
2026-05-06 00:33:31 +0900

    회원가입 API 추가

--- src/main/java/com/cos/jwt/controller/RestApiController.java
+++ src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,13 +1,36 @@
 package com.cos.jwt.controller;
 
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApiController {
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+
+    public RestApiController(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/home")
     public String home() {
         return "<h1>home</h1>";
+    }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+
+        return "회원가입 완료";
     }
 }
