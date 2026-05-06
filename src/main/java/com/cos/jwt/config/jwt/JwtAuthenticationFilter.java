0b62595f12c989b0e4f8fe0553cfc358df0d79f8
yuna1313 <yuna38748435@gmail.com>
2026-05-06 23:09:02 +0900

    JwtAuthenticationFilter 인증 시도 로직 추가

--- src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,23 +1,65 @@
 package com.cos.jwt.config.jwt;
 
+import com.cos.jwt.config.auth.PrincipalDetails;
+import com.cos.jwt.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
+
 // POST /login을 요청하여 id, pw 전달하면 UsernamePasswordAuthenticationFilter가 동작을 함
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
-    // /long 요청을 하면 로그인 시도를 위해 실행되는 함수
+    // /login 요청을 하면 로그인 시도를 위해 실행되는 함수
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        System.out.println("[JwtAuthenticationFilter] 로그인 시도 중");
-        return super.attemptAuthentication(request, response);
+        log.info("[JwtAuthenticationFilter] 로그인 시도 중");
+
+        // 1. username, password 받기
+        try {
+//            BufferedReader bufferedReader = request.getReader();
+//
+//            String input;
+//            while((input = bufferedReader.readLine()) != null) {
+//                System.out.println(input);
+//            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            User user = objectMapper.readValue(request.getInputStream(), User.class);
+            log.info("[JwtAuthenticationFilter] User: {}", user);
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            // PrincipalDetailsService의 loadUserByUsername() 함수가 실행된 후 정상이면 authentication에 로그인 정보가 담김
+            // DB에 있는 username과 password가 일치
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            // 로그인이 되었다는 것을 의미
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+            log.info("[JwtAuthenticationFilter] principalDetails: {}", principalDetails.getUser().getUsername());
+
+            // authentication 객체가 session 영역에 저장
+            // authentication return의 이유는 권한 관리를 security가 대신 해주기 때문
+            return authentication;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
